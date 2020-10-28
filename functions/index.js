2687ee2cb0b50a85f3bc701d8e4cd2a1e24f1e15
Abyrd9 <andrew.byrd99@gmail.com>
2020-10-28 13:10:04 -0700

    placing hooks and functions into their own index files

---
+++ functions/index.js
@@ -0,0 +1,1 @@
+export { default as checkIsOutsideContainer } from './checkIsOutsideContainer';
---
+++ hooks/index.js
@@ -0,0 +1,4 @@
+export { default as useDeepCompareEffect } from './useDeepCompareEffect';
+export { default as useMediaQuery } from './useMediaQuery';
+export { default as useMountValueEffect } from './useMountValueEffect';
+export { default as useWindowResizeEffect } from './useWindowResizeEffect';
